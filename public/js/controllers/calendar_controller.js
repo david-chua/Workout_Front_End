ea85a379e1191f2451dd4c67ca2d441def57547f
dp_chua <dp_chua@hotmail.com>
2017-03-18 01:53:28 -0400

    calendar controller might be working?

--- public/js/controllers/calendar_controller.js
+++ public/js/controllers/calendar_controller.js
@@ -1,3 +1,88 @@
-function CalendarController(){
-  console.log('CalendarController')
+function CalendarController($http, $uibModal, $uiCalendarConfig){
+  var server = 'http://localhost:3000'
+
+  this.animationsEnabled = true;
+  this.events = [];
+  events = this.events;
+  eventSources = this.eventSources;
+
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+  //
+  // Get route to get all the workouts on the calendar.
+
+  function getWorkouts(){
+    $http.get(`${server}/users/${currentUser._id}/workouts`)
+      .then(function(response){
+        console.log(response.data.data)
+        this.events = response.data.data
+      })
+  }
+
+  this.eventSources = [this.events];
+
+  var calendar = document.getElementById('workoutcalendar');
+
+
+
+// Angular UI Calendar Configuration
+
+this.uiConfig = {
+  calendar: {
+    height: 650,
+    editable: true,
+    selectable: true,
+    header: {
+      right: 'today yesterday,tomorrow'
+    },
+    views: {
+      month: {
+        colunFormat: 'dddd'
+      }
+    },
+    dayClick: function(data){
+      var thisDate = date;
+      // modal instance service: modal directive using bootstrap
+      var $uiModalInstance = $uiModal.open({
+        templateUrl: 'myAddModalContent.html',
+        controller: 'AddModalInstanceCtrl',
+        // using uiModal to create view templates in which can be called in the HTML file
+        controllerAs: 'addModal',
+        resolve: {
+          thisDate: thisDate
+        }
+      });
+      console.log(date);
+    },
+
+    eventClick: function(selectedWorkout){
+      this.selectedWorkout = selectedWorkout;
+      console.log(selectedWorkout);
+      var $uiModalInstance = $uiModal.open({
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        controllerAs: 'modal',
+        resolve: {
+          selectedWorkout: selectedWorkout
+        }
+      });
+    },
+    // triggered when dragging stops and the event has moved to a different day/time
+    // fullcalendar.io/docs/event_ui/eventDrop
+    eventDrop: function(selectedWorkout, delta, revertFunc, jsEvent,ui,view){
+      this.selectedWorkout = selectedWorkout;
+      console.log(selectedWorkout);
+      console.log(selectedWorkout.start._d);
+      var newDate = selectedWorkout.start._d;
+      $http.put(`${server}/${currentUser._id}/workouts/${selectedWorkout.id}`)
+      data: {
+        start: newDate
+      }
+    }
+
+
+    }
+  }
 }
--- public/js/controllers/workout_controller.js
+++ public/js/controllers/workout_controller.js
@@ -1,3 +1,19 @@
-function WorkoutController(){
+function WorkoutController($http, $uibModalInstance, thisDate){
+  //thisDate from Calendar Controller
   console.log('WorkoutController')
+  var server = "http://localhost:3000"
+  this.addEventData = {};
+  this.events = [];
+  this.thisDate = thisDate.format('MMMM Do');
+
+
+  $http.post(`${server}/users/${currentUser._id}/workouts`){
+  // data = this.addEventData
+  //   }.then(function(response){
+  //   console.log(response.data);
+  //   events.push(response.data);
+  //   this.addEventData = {};
+  //   eventSources = [events];
+  // }.bind(this));
+
 }
