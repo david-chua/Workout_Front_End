0c0dc54c927b8474b6041e1c231777f062327caf
dp_chua <dp_chua@hotmail.com>
2017-03-19 00:16:14 -0400

    get Workouts are working

--- public/js/controllers/workout_controller.js
+++ public/js/controllers/workout_controller.js
@@ -4,16 +4,28 @@ function WorkoutController($http, $state, $scope){
   var server = "http://localhost:3000"
   var self = this;
 
-
+//create a new workout for the user
   function createWorkout(workout){
       $http.post(`${server}/workouts`, {workout: workout})
       .then(function(res){
         console.log(res);
 
-        $state.go('home')
+        $state.reload('home')
       });
 }
   self.createWorkout = createWorkout;
+
+
+//get all workout of that user
+  function getWorkout(){
+    $http.get(`${server}/workouts`)
+    .then(function(res){
+      self.allWorkout = res.data.workouts;
+      console.log(self.allWorkout);
+    })
+  }
+  self.getWorkout = getWorkout;
+  getWorkout();
 
 
   // function getWorkouts(){
